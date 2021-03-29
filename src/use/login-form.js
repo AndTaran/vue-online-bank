0df09fb99af0fb274d53810b137c81911faf8ff8
AndTaran <65190142+AndTaran@users.noreply.github.com>
2021-03-29 18:49:18 +0300

    Initial commit

---
+++ src/use/login-form.js
@@ -0,0 +1,58 @@
+import { computed, watch } from "vue";
+import * as yup from "yup";
+import { useField, useForm } from "vee-validate";
+
+export function useLoginForm() {
+  const { handleSubmit, isSubmitting, submitCount } = useForm();
+
+  const { value: email, errorMessage: eError, handlerBlur: eBlur } = useField(
+    "email",
+    yup
+      .string()
+      .trim()
+      .required("Введите email")
+      .email("Введите корректный email")
+  );
+
+  const PASSWORD_MIN_LENGTH = 6;
+
+  const {
+    value: password,
+    errorMessage: pError,
+    handlerBlur: pBlur,
+  } = useField(
+    "password",
+    yup
+      .string()
+      .trim()
+      .required("Введите пароль")
+      .min(
+        PASSWORD_MIN_LENGTH,
+        `Пароль не может быть меньше ${PASSWORD_MIN_LENGTH} символов`
+      )
+  );
+
+  const isToManyAttempts = computed(() => submitCount.value >= 3);
+
+  watch(isToManyAttempts, (val) => {
+    if (val) {
+      setTimeout(() => (submitCount.value = 0), 2000);
+    }
+  });
+
+  const onSubmit = handleSubmit((values) => {
+    console.log("Form", values);
+  });
+
+  return {
+    email,
+    password,
+    eError,
+    pError,
+    eBlur,
+    pBlur,
+    onSubmit,
+    isSubmitting,
+    isToManyAttempts,
+  };
+}
