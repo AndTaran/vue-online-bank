87092f6f71c372265e23eaee43141fad4e7cfbcf
AndTaran <65190142+AndTaran@users.noreply.github.com>
2021-04-02 15:43:13 +0300

    feat: added component help
    and build a project

---
+++ dist/precache-manifest.9695c171823696fa7151cbce06cb583a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baed3f3d213763714b8e",
+    "url": "/css/app.37adbbe3.css"
+  },
+  {
+    "revision": "5aa56edfefe1b17e92c13b0f77a469e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baed3f3d213763714b8e",
+    "url": "/js/app.d5ef7073.js"
+  },
+  {
+    "revision": "0422d63e619fa06adec2",
+    "url": "/js/chunk-2d0a4783.779ea9fe.js"
+  },
+  {
+    "revision": "fcb68611314ab9b2b9ac",
+    "url": "/js/chunk-2d0be70b.aaf0856a.js"
+  },
+  {
+    "revision": "58c236f5141ac94e4b99",
+    "url": "/js/chunk-2d216dc7.03ef2144.js"
+  },
+  {
+    "revision": "a773afeaa32c202df6ea",
+    "url": "/js/chunk-vendors.476ace7a.js"
+  },
+  {
+    "revision": "c8543f43cd7c3ccbc7ce8d425b951a5b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page back title="Ответы на вопросы">
+    <question-item
+      v-for="q in questions"
+      :key="q.title"
+      :question="q"
+    ></question-item>
+  </app-page>
 </template>
+
+<script>
+import AppPage from "../components/ui/AppPage";
+import QuestionItem from "../components/question/QuestionItem";
+
+export default {
+  setup() {
+    const questions = [
+      { title: "Как создать заявку", text: "Очень просто))" },
+      { title: "Как удалить заявку", text: "Очень сложно))" },
+    ];
+
+    return { questions };
+  },
+  components: { AppPage, QuestionItem },
+};
+</script>
+
+<style></style>
